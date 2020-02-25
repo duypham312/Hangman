69a09bd77f19d8afea65478eb18bfc52cbe52416
Duy <worthlessduy@gmail.com>
2020-02-25 12:19:50 +0700

    Initial Commit

---
+++ Hangman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hangman
+//
+//  Created by Duy on 2/25/20.
+//  Copyright © 2020 Duy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
