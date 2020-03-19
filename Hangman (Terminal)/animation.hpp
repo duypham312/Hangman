6644f78b7a69504b9004c29dde9ea003265bbbdc
Duy <worthlessduy@gmail.com>
2020-03-19 17:09:24 +0700

    Hangman - Terminal

---
+++ Hangman (Terminal)/animation.hpp
@@ -0,0 +1,18 @@
+//
+//  animation.hpp
+//  Hangman
+//
+//  Created by Duy on 3/18/20.
+//  Copyright © 2020 Duy. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+void clear();
+void defeat();
+void victory();
